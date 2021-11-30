5752ff1ba6182dc58bc7e663ce31cfd2128e8a04
Christian Glassiognon <christian@heyglassy.co>
2021-11-30 13:17:24 -0700

    Cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,6 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 
 export default withTRPC<AppRouter>({
   config({ ctx }) {
-    console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
     const url = process.env.NEXT_PUBLIC_VERCEL_URL
       ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
       : "http://localhost:3000/api/trpc";
